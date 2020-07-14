61335432dd9f0e664c47db0f4966dbd341e73fe4
Mrnobody <mrcartoonster@icloud.com>
2020-07-14 14:18:18 +0000

    📦 NEW: TYP-3 #time 30m #comment passing function.

--- tests/test_typer_passwd.py
+++ tests/test_typer_passwd.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 import pytest
 from typer.testing import CliRunner
+
 from typer_passwd import __version__
 from typer_passwd.main import app
 
 runner = CliRunner()
+
 
 def test_version():
     """Checking version is set correclty."""
@@ -16,4 +18,4 @@ def test_amount():
     """Check length of password."""
     result = runner.invoke(app, ["10"])
     assert result.exit_code == 0
-    assert 10 == len(result.stdout)
+    assert 11 == len(result.stdout)
--- typer_passwd/main.py
+++ typer_passwd/main.py
@@ -1,16 +1,22 @@
+# -*- coding: utf-8 -*-
 import typer
 from yakutils import random_string
 
 app = typer.Typer()
 
 
-@app.callback
-def passwd(value: int):
+def callback_passwd(value: int) -> str:
     """Function that return random strings."""
     return random_string(value)
 
 
 @app.command()
-def main(amount: str = typer.Option(..., callback=passwd)):
+def main(amount: int = typer.Argument(..., callback=callback_passwd)):
     """Option that returns the password."""
     typer.echo(amount)
+
+
+# Placing main while developing
+if __name__ == "__main__":
+    app()
+    # typer.run(main)
